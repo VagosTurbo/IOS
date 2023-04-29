2ea6db704a31504f39217b62a267272a79d0a018
VagosTurbo <semanco.boris@gmail.com>
2023-04-29 23:53:59 +0200

    almost finished

--- proj2/proj2.c
+++ proj2/proj2.c
@@ -4,72 +4,47 @@
 * @project: Projekt 2
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/sem.h>
-#include <sys/shm.h>
-#include <unistd.h>
-#include <semaphore.h>
-#include <string.h>
-#include <fcntl.h>
-#include <stdarg.h>
-#include <limits.h>
-#include <stdbool.h>
-#include <time.h>
+#include "proj2.h"
 
-#define EXIT_SUCCESS 0
-#define EXIT_FAILURE 1
+/**
+ * @todo: opravit cas posty <f/2, f>
+ * osetrit lepsie argumenty +
+ * urobit .h subor
+ * errory musia uvolnovat pamat
+ * errory aj pre init semaforoch +
+ * okomentovat veci
+*/
 
-sem_t *mutex;
-sem_t *queue_done;
-sem_t *queue2_done;
-sem_t *queue3_done;
-sem_t *customer_done;
-sem_t *employee_done;
-sem_t *queue;
-sem_t *queue2;
-sem_t *queue3;
-sem_t *queue1;
-FILE *file;
-int *action_counter;
-int *post_office;
-int *people_inside;
-int *queue_size;
-int *queue_size2;
-int *queue_size3;
-
-typedef struct{
-    int CustomerID;
-    int CustomerWaitTime;
-    int CustomerDemand;
-} Customer; 
 
 // prints help how to use the program
 void help(){
-    printf("Usage: ./proj2 NZ NU TZ TU F\n");
-    printf("NZ: počet zákazníků\n");
-    printf("NU: počet úředníků\n");
-    printf("TZ: Maximální čas v milisekundách, po který zákazník po svém vytvoření čeká, než vejde na poštu. 0<=TZ<=10000\n");
-    printf("TU: Maximální délka přestávky úředníka v milisekundách. 0<=TU<=100\n");
-    printf("F: Maximální čas v milisekundách, po kterém je uzavřena pošta pro nově příchozí. 0<=F<=10000\n");
+    fprintf(stderr, "Usage: ./proj2 NZ NU TZ TU F\n");
+    fprintf(stderr, "NZ: počet zákazníků\n");
+    fprintf(stderr, "NU: počet úředníků\n");
+    fprintf(stderr, "TZ: Maximální čas v milisekundách, po který zákazník po svém vytvoření čeká, než vejde na poštu. 0<=TZ<=10000\n");
+    fprintf(stderr, "TU: Maximální délka přestávky úředníka v milisekundách. 0<=TU<=100\n");
+    fprintf(stderr, "F: Maximální čas v milisekundách, po kterém je uzavřena pošta pro nově příchozí. 0<=F<=10000\n");
 }
 
 // checks if the number of arguments is correct
 int argcheck(int argc, char *argv[]) {
 
-    int NZ = atoi(argv[1]);
-    int NU = atoi(argv[2]);
-    int TZ = atoi(argv[3]);
-    int TU = atoi(argv[4]);
-    int F = atoi(argv[5]);
-
     if (argc != 6) {
-        help();
         fprintf(stderr, "Incorrect number of arguments \n");
+        help();
+        return EXIT_FAILURE;
+    }
+    
+    // checks if arguments are numbers
+    char *a, *b, *c, *d, *e;
+    int NZ = strtol(argv[1], &a, 10);
+    int NU = strtol(argv[2], &b, 10);
+    int TZ = strtol(argv[3], &c, 10);
+    int TU = strtol(argv[4], &d, 10);
+    int F = strtol(argv[5], &e, 10);
+
+    if (*a != '\0' || *b != '\0' || *c != '\0' || *d != '\0' || *e != '\0') {
+        fprintf(stderr, "Incorrect format of arguments\n");
         return EXIT_FAILURE;
     }
 
@@ -117,10 +92,23 @@ void startup(){
     queue_size = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     queue_size2 = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     queue_size3 = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    queue1 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
-    //TODO DOPLNIT
-    if (mutex == MAP_FAILED ||  customer_done == MAP_FAILED || employee_done == MAP_FAILED || action_counter == MAP_FAILED || post_office == MAP_FAILED || queue == MAP_FAILED || queue2 == MAP_FAILED || queue3 == MAP_FAILED || queue_done == MAP_FAILED || queue2_done == MAP_FAILED || queue3_done == MAP_FAILED || queue_size == MAP_FAILED || queue_size2 == MAP_FAILED || queue_size3 == MAP_FAILED || queue1 == MAP_FAILED){
+    // checks if shared variables were created correctly
+    if (mutex == MAP_FAILED 
+    ||  customer_done == MAP_FAILED 
+    ||  employee_done == MAP_FAILED 
+    ||  action_counter == MAP_FAILED 
+    ||  post_office == MAP_FAILED 
+    ||  queue == MAP_FAILED 
+    ||  queue2 == MAP_FAILED 
+    ||  queue3 == MAP_FAILED 
+    ||  queue_done == MAP_FAILED 
+    ||  queue2_done == MAP_FAILED
+    ||  queue3_done == MAP_FAILED 
+    ||  queue_size == MAP_FAILED 
+    ||  queue_size2 == MAP_FAILED 
+    ||  queue_size3 == MAP_FAILED )
+    {
         fprintf(stderr, "Error creating shared memory\n");
         exit(EXIT_FAILURE);
     }
@@ -169,6 +157,9 @@ void cleanup(){
     munmap(queue, sizeof(sem_t));
     munmap(queue2, sizeof(sem_t));
     munmap(queue3, sizeof(sem_t));
+    munmap(queue_size, sizeof(int));
+    munmap(queue_size2, sizeof(int));
+    munmap(queue_size3, sizeof(int));
 
     // close file
     if(file != NULL){
@@ -251,7 +242,7 @@ void employee_process(int EmployeeID, int breaktime){
             sem_post(mutex);
             sem_wait(queue_done);
             sem_wait(mutex);
-            fprintf(file, "%d: U %d: serving service 1\n", ++(*action_counter), EmployeeID);
+            fprintf(file, "%d: U %d: serving a service of type 1\n", ++(*action_counter), EmployeeID);
             sem_post(mutex);
             usleep((rand() % 10)*1000);
             sem_wait(mutex);
@@ -266,7 +257,7 @@ void employee_process(int EmployeeID, int breaktime){
             sem_post(mutex);
             sem_wait(queue2_done);
             sem_wait(mutex);
-            fprintf(file, "%d: U %d: serving service 2\n", ++(*action_counter), EmployeeID);
+            fprintf(file, "%d: U %d: serving a service of type 2\n", ++(*action_counter), EmployeeID);
             sem_post(mutex);
             usleep((rand() % 10)*1000);
             sem_wait(mutex);
@@ -282,7 +273,7 @@ void employee_process(int EmployeeID, int breaktime){
             sem_post(mutex);
             sem_wait(queue3_done);
             sem_wait(mutex);
-            fprintf(file, "%d: U %d: serving service 3\n", ++(*action_counter), EmployeeID);
+            fprintf(file, "%d: U %d: serving a service of type 3\n", ++(*action_counter), EmployeeID);
             sem_post(mutex);
             usleep((rand() % 10)*1000);
             sem_wait(mutex);
@@ -315,7 +306,7 @@ int main(int argc, char *argv[]) {
 
     
     // checks if arguments are in correct format
-    if (argcheck(argc, argv) == EXIT_FAILURE){
+    if (argcheck(argc, argv)){
         return EXIT_FAILURE;
     }
     int CustomerCount = atoi(argv[1]);
@@ -335,6 +326,11 @@ int main(int argc, char *argv[]) {
         (*post_office) = 1;
         exit(EXIT_SUCCESS);
     }
+    // check if fork failed
+    else if (pid_post_office < 0){
+        fprintf(stderr, "fork failed\n");
+        return EXIT_FAILURE;
+    }
 
     for (int i = 0; i < EmployeeCount; i++){
         pid_t pid_e_id = fork();
@@ -342,6 +338,11 @@ int main(int argc, char *argv[]) {
             int breaktime = rand() % EmployeeMaxBreak;
             employee_process(i+1, breaktime);
             exit(EXIT_SUCCESS);
+        }
+        // check if fork failed
+        else if (pid_e_id < 0){
+            fprintf(stderr, "fork failed\n");
+            return EXIT_FAILURE;
         }
     }
 
@@ -355,8 +356,13 @@ int main(int argc, char *argv[]) {
             customer_process(customer);
             exit(EXIT_SUCCESS);
         }
+        // check if fork failed
+        else if (pid_c_id < 0){
+            fprintf(stderr, "fork failed\n");
+            return EXIT_FAILURE;
+        }
     }
-    cleanup();
     while( wait(NULL) > 0);
+    cleanup();
     return EXIT_SUCCESS;
 }
---
+++ proj2/proj2.h
@@ -0,0 +1,65 @@
+
+// @author: xseman06
+// @date: 2023-04-20
+// @project: Projekt 2
+
+#ifndef PROJ2_H
+#define PROJ2_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+sem_t *mutex;
+sem_t *customer_ready;
+sem_t *employee_ready;
+sem_t *customer_done;
+sem_t *employee_done;
+sem_t *queue;
+sem_t *queue2;
+sem_t *queue3;
+sem_t *queue_done;
+sem_t *queue2_done;
+sem_t *queue3_done;
+
+
+FILE *file;
+int *action_counter;
+int *post_office;
+int *people_inside;
+int *queue_size;
+int *queue_size2;
+int *queue_size3;
+
+typedef struct{
+    int CustomerID;
+    int CustomerWaitTime;
+    int CustomerDemand;
+} Customer; 
+
+
+// Functions
+void help();
+int argcheck(int argc, char *argv[]);
+void startup();
+void cleanup();
+void customer_process(Customer customer);
+void employee_process(int EmployeeID, int breaktime);
+
+#endif // PROJ2_H
